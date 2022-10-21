ed72b0ec5439ee355ca2b8fa1b9db7d3d538480a
Daalu05 <arinzechimdalu05@gmail.com>
2022-10-21 00:36:39 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - print num from 0-9
- * Return: void
+ * print_numbers - function thaat print all number 0 to 9
+ *
+ * Return: returns void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar ('\n');
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar(10);
 }
