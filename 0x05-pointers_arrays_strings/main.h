79d6f60a945e4ecd4990a1bd9b1fd663ec93799c
Daalu05 <arinzechimdalu05@gmail.com>
2022-10-25 05:46:45 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN.H
 #define MAIN.H
 
+/**
+ * protypes files
+ * import from here
+ */
+int _putchar(char.c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
