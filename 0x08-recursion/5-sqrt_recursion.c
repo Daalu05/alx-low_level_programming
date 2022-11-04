9ae6a8c3e8214c5200e4b989e30016ee65a85d47
Daalu05 <arinzechimdalu05@gmail.com>
2022-11-03 18:20:21 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * _evaluate - evaluate function sqrt
- * @i: integer
- * @n: integer
- * Return: evaluate sqrt
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integere
+ * Return: sqrt_recursion
  */
-int _evaluate(int i, int n)
+int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
-	return (n);
+	int i = 0;
 
-	else if (i * i < n)
-	return (_evaluate(i + 1, n));
-
-	else if (i * i == n)
-	return (i);
-
+	if (i < 0)
 	return (-1);
 
-	return (-1);
+	elsee
+	{
+	return (_evaluate(i, n));
+	}
 }
