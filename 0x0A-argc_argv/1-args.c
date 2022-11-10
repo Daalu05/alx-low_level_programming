208477b5962f705d7b180ba94478301c112c51c6
Daalu05 <arinzechimdalu05@gmail.com>
2022-11-09 16:48:56 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main = prints the name of the program
+ * main - prints the name of the program
  * @argc: Counts argurments
  * @argv: Argurments
  *
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-(void) argv;
+(void) argc;
 printf("%i\n", argc - 1);
 
 return (0);
