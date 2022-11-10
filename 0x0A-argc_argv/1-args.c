4680844b4a13ea363f72e2d9b2937a78504cff93
Daalu05 <arinzechimdalu05@gmail.com>
2022-11-09 16:43:50 -0800

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main = prints the name of the program
+ * @argc: Counts argurments
+ * @argv: Argurments 
+ *
+ * Return: Always 0 (Success);
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%i\n", argc - 1)  ;
+
+return(0);
+}
